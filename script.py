1d60b09fc6261436734ef2cd66e6f86a9938ef65
Maxime Cruzel <84211047+mcruzel@users.noreply.github.com>
2022-11-16 11:31:54 +0100

    Update : rest format, feedback on queue

--- script.py
+++ script.py
@@ -1,23 +1,31 @@
 # Maxime Cruzel
 # Role system assign
-# V 1.0, 2022, July
+# V 1.1, 2022, November
+# This script :
+# 1) Read users from a specific system cohort (cohortid)
+# 2) Affect those users to a specific role (roleid, contextid)
+# 3) (optionnal because can be easily removed) Apply some rules to users from the specific cohort
 
 import requests
 import json
+import re
 
-token_ws_cohort_reading = "xxxxxx"
-token_ws_role_assign = "xxxxx"
-url_moodle = "https://xxx"
+login = ""
+password = ""
 cohortid = 0 # cohort members of the cohort whose members will be added to the system role
 roleid = 0 # the id of the role which you want to add members from the aimed cohort
 userid = 0 # doesn't matter
-contextid = 1 # use 1 for system-wide roles
+contextid = 0 # use 1 for system-wide roles
+
+url_moodle = ""
 
 
 # Import users id from a system cohort
-def import_usersid():
+def import_userid():
     global webservice_reponse_content_py_tableau
-    webservice_userid_response_content = url_moodle+"/webservice/rest/server.php?wstoken="+token_ws_cohort_reading+"&wsfunction=core_cohort_get_cohort_members&moodlewsrestformat=json&cohortids[0]="+str(cohortid)
+    ws = "core_cohort_get_cohort_members"
+    param_request = "&cohortids[0]="+str(cohortid)
+    webservice_userid_response_content = url_moodle+"webservice/rest/simpleserver.php?wsusername="+str(login)+"&wspassword="+str(password)+"&moodlewsrestformat=json&wsfunction="+ws+param_request
     webservice_reponse_content = requests.get(webservice_userid_response_content)
     webservice_reponse_content_py = json.loads(webservice_reponse_content.text)
     webservice_reponse_content_py_tableau =  []
@@ -26,15 +34,43 @@ def import_usersid():
         webservice_reponse_content_py_tableau = i['userids']
         
     print(webservice_reponse_content_py_tableau)
+    return len(webservice_reponse_content_py_tableau)
 
 # Role assign to cohort members
-def assign_members_to_role():
+def assign_members_to_role(total_count):
+    count = 1
     for i in webservice_reponse_content_py_tableau:
         userid = i
-        webservice_role_assign = url_moodle+"/webservice/rest/server.php?wstoken="+token_ws_role_assign+"&wsfunction=core_role_assign_roles&moodlewsrestformat=json&assignments[0][roleid]="+str(roleid)+"&assignments[0][userid]="+str(userid)+"&assignments[0][contextid]="+str(contextid)
-        print(webservice_role_assign)
+        ws = "core_role_assign_roles"
+        param_request = "&assignments[0][roleid]="+str(roleid)+"&assignments[0][userid]="+str(userid)+"&assignments[0][contextid]="+str(contextid)
+        webservice_role_assign = url_moodle+"webservice/rest/simpleserver.php?wsusername="+str(login)+"&wspassword="+str(password)+"&moodlewsrestformat=json&wsfunction="+ws+param_request
         webservice_role_assign_post = requests.post(webservice_role_assign)
-    print("Travail terminé")    
+        print(f"{count}/{total_count} -> {webservice_role_assign_post}")
+        count += 1
         
-import_usersid()
-assign_members_to_role()
+        rules_on_name(userid) # can be deleted
+
+# apply rules on teachers/ADM usernames
+def rules_on_name(userid):
+    ws = "core_user_get_users"
+    param_request = "&criteria[0][key]=id&criteria[0][value]="+str(userid)
+    webservice_request = url_moodle+"webservice/rest/simpleserver.php?wsusername="+str(login)+"&wspassword="+str(password)+"&moodlewsrestformat=json&wsfunction="+ws+param_request
+    webservice_reponse_content = requests.get(webservice_request)
+    webservice_reponse_content_py = json.loads(webservice_reponse_content.text)
+    firstname = webservice_reponse_content_py['users'][0]['firstname']
+    # eliminer, par regex, le numéro dans le prénom des homonymes
+    pattern = r'[0-9]'
+    firstname = re.split(pattern, firstname)[0]
+    lastname = webservice_reponse_content_py['users'][0]['lastname']
+    firstnameC = firstname.capitalize()
+    lastnameC = lastname.upper()
+    ws = "core_user_update_users"
+    param_request = "&users[0][id]="+str(userid)+"&users[0][firstname]="+firstnameC+"&users[0][lastname]="+lastnameC
+    webservice_request = url_moodle+"webservice/rest/simpleserver.php?wsusername="+str(login)+"&wspassword="+str(password)+"&moodlewsrestformat=json&wsfunction="+ws+param_request
+    webservice_reponse_content = requests.get(webservice_request)
+    
+    
+total_count = import_userid()
+assign_members_to_role(total_count)
+
+print("Travail terminé")
